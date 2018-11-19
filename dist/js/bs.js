2b3634925d19cb0b3e5890495d5bcb3eac47fa81
tdejoye@eukles.com <tdejoye@eukles.com>
2018-11-19 16:50:58 +0100

    Change the "error" string to "oups"

--- dist/js/bs.js
+++ dist/js/bs.js
@@ -7517,212 +7517,199 @@ Bs.define('Bs.View.Collection.Item.Restore', {
 "use strict";
 
 Bs.define('Bs.View.Alert', {
-	extend       : 'Bs.View',
-	hasStylesheet: false,
-	autoMask     : false,
-	elStyle      : {position: 'relative'},
-	elClass      : "bootstrap",
-	options      : {
-		translate      			: true,
-		translateTitle 			: true,
-		icon           			: null,
-		type           			: 'info',
-		title          			: null,
-		hasTitle       			: true,
-		msg            			:	null,
-		view           			: null,
-		viewOptions    			: null,
-		dismissible    			: true,
-		autoDismissible			: false,
-		cancelDismissOnHover: true,
-		delay          			: 4000
-	},
-	data					: {
-		idTimeout : null
-	},
+  extend       : 'Bs.View',
+  hasStylesheet: false,
+  autoMask     : false,
+  elStyle      : {position: 'relative'},
+  elClass      : "bootstrap",
+  options      : {
+    translate      : true,
+    translateTitle : true,
+    icon           : null,
+    type           : 'info',
+    title          : null,
+    hasTitle       : true,
+    msg            : null,
+    view           : null,
+    viewOptions    : null,
+    dismissible    : true,
+    autoDismissible: false,
+    delay          : 4000
+  },
 
-	initialize: function () {
-		var me = this;
-		me.callParent("initialize");
-		if (!me.options.title) {
-			if (me.options.type === 'default') {
-				me.options.hasTitle = false;
-			}
-			else {
-				me.options.title = me.options.type + '.title';
-			}
-		}
+  initialize: function () {
+    var me = this;
+    me.callParent("initialize");
+    if (!me.options.title) {
+      if (me.options.type === 'default') {
+        me.options.hasTitle = false;
+      }
+      else {
+        me.options.title = me.options.type + '.title';
+      }
+    }
 
-		if (!me.options.icon) {
-			switch (me.options.type) {
-				case 'danger':
-					me.options.icon = 'fa fa-exclamation-circle';
-					break;
-				case 'warning':
-					me.options.icon = 'fa fa-exclamation-triangle';
-					break;
-				case 'success':
-					me.options.icon = 'fa fa-check';
-					break;
-				case 'info':
-					me.options.icon = 'fa fa-info-circle';
-					break;
-				default:
-					me.options.icon = "fa fa-" + me.options.type;
-			}
-		}
-		if (me.options.type === 'danger') {
-			me.options.autoDismissible = false;
-		}
-		if (me.options.view) {
-			me.options.translate = false;
-		}
-		else {
-			if (me.options.msg instanceof jQuery) {
-				me.options.msg = $('<div></div>').html(me.options.msg).html();
-				me.options.translate = false;
-			}
-			else {
-				if ($.isArray(me.options.msg)) {
-					me.options.msg = me.options.msg[0].urlPath + ':' + me.options.msg[1]
-				}
-				else if (!me.options.msg) {
-					if (me.options.type === "success") {
-						me.options.msg = Bs.Lang.t("success_");
-					}
-					else {
-						me.options.msg = Bs.Lang.t('unknown_error');
-					}
-				}
-			}
-		}
-		if ($.isArray(me.options.title)) {
-			me.options.title = me.options.title[0].urlPath + ':' + me.options.title[1]
-		}
-	},
-
-	render: function () {
-		var me = this;
-		me.callParent("render");
-		me.$el.hide();
-
-		if (me.options.view) {
-			Bs.require(me.options.view, function (View) {
-				var view,
-					viewOptions = me.options.viewOptions || {};
-
-				viewOptions.renderTo = me.$el.find('.content');
-				view = new View(viewOptions);
-				me.renderCallback();
-				view.on('close', function () {
-					me.trigger('close');
-				});
-			})
-		}
-		me.renderCallback();
-		me.trigger("ready");
-	},
-
-	renderCallback: function () {
-		var me = this;
-
-		me.$el.slideDown(function () {
-			me.trigger('shown');
-			me.$el.find('.alert-icon').fadeIn();
-		});
-		if (me.options.autoDismissible) {
-			me.data.idTimeout = setTimeout(function () {
-				me.close();
-			}, me.options.delay)
-			if(me.options.cancelDismissOnHover) {
-				me.$el.on('mouseover', function() {
-					if(me.data.idTimeout) {
-            clearTimeout(me.data.idTimeout)
-            me.data.idTimeout = null;
+    if (!me.options.icon) {
+      switch (me.options.type) {
+        case 'danger':
+          me.options.icon = 'fa fa-exclamation-circle';
+          break;
+        case 'warning':
+          me.options.icon = 'fa fa-exclamation-triangle';
+          break;
+        case 'success':
+          me.options.icon = 'fa fa-check';
+          break;
+        case 'info':
+          me.options.icon = 'fa fa-info-circle';
+          break;
+        default:
+          me.options.icon = "fa fa-" + me.options.type;
+      }
+    }
+    if (me.options.type === 'danger') {
+      me.options.autoDismissible = false;
+    }
+    if (me.options.view) {
+      me.options.translate = false;
+    }
+    else {
+      if (me.options.msg instanceof jQuery) {
+        me.options.msg = $('<div></div>').html(me.options.msg).html();
+        me.options.translate = false;
+      }
+      else {
+        if ($.isArray(me.options.msg)) {
+          me.options.msg = me.options.msg[0].urlPath + ':' + me.options.msg[1]
+        }
+        else if (!me.options.msg) {
+          if (me.options.type === "success") {
+            me.options.msg = Bs.Lang.t("success_");
           }
-				})
-			}
-		}
-	},
+          else {
+            me.options.msg = Bs.Lang.t('unknown_error');
+          }
+        }
+      }
+    }
+    if ($.isArray(me.options.title)) {
+      me.options.title = me.options.title[0].urlPath + ':' + me.options.title[1]
+    }
+  },
 
-	close: function (e) {
-		if (e) {
-			e.preventDefault();
-			e.stopPropagation();
-		}
-		var me = this;
-		me.$el.find('.alert-icon').hide();
-		this.$el.slideUp(function () {
-			me.$el.remove();
-		});
-		return false;
-	},
+  render: function () {
+    var me = this;
+    me.callParent("render");
+    me.$el.hide();
 
-	events: {
-		"click .close": "close"
-	}
+    if (me.options.view) {
+      Bs.require(me.options.view, function (View) {
+        var view,
+          viewOptions = me.options.viewOptions || {};
+
+        viewOptions.renderTo = me.$el.find('.content');
+        view = new View(viewOptions);
+        me.renderCallback();
+        view.on('close', function () {
+          me.trigger('close');
+        });
+      })
+    }
+    me.renderCallback();
+    me.trigger("ready");
+  },
+
+  renderCallback: function () {
+    var me = this;
+
+    me.$el.slideDown(function () {
+      me.trigger('shown');
+      me.$el.find('.alert-icon').fadeIn();
+    });
+    if (me.options.autoDismissible) {
+      setTimeout(function () {
+        me.close();
+      }, me.options.delay)
+    }
+  },
+
+  close: function (e) {
+    if (e) {
+      e.preventDefault();
+      e.stopPropagation();
+    }
+    var me = this;
+    me.$el.find('.alert-icon').hide();
+    this.$el.slideUp(function () {
+      me.$el.remove();
+    });
+    return false;
+  },
+
+  events: {
+    "click .close": "close"
+  }
 
 }, function () {
-	Bs.View.Alert.error = function (msg, renderTo, translated) {
-		return new Bs.View.Alert({
-			type     : 'danger',
-			title    : 'danger.title',
-			msg      : msg || 'danger.msg.default',
-			renderTo : renderTo || 'body',
-			translate: !translated
-		});
-	};
+  Bs.View.Alert.error = function (msg, renderTo, translated) {
+    return new Bs.View.Alert({
+      type     : 'danger',
+      title    : 'danger.title',
+      msg      : msg || 'danger.msg.default',
+      renderTo : renderTo || 'body',
+      translate: !translated
+    });
+  };
 
-	Bs.View.Alert.warning = function (msg, renderTo, translated) {
-		return new Bs.View.Alert({
-			type     : 'warning',
-			title    : 'warning.title',
-			msg      : msg,
-			renderTo : renderTo || 'body',
-			translate: !translated
-		});
-	};
+  Bs.View.Alert.warning = function (msg, renderTo, translated) {
+    return new Bs.View.Alert({
+      type     : 'warning',
+      title    : 'warning.title',
+      msg      : msg,
+      renderTo : renderTo || 'body',
+      translate: !translated
+    });
+  };
 
-	Bs.View.Alert.success = function (msg, renderTo, translated) {
-		return new Bs.View.Alert({
-			type     : 'success',
-			title    : 'success.title',
-			msg      : msg || 'success.msg.default',
-			renderTo : renderTo || 'body',
-			translate: !translated
-		});
-	};
+  Bs.View.Alert.success = function (msg, renderTo, translated) {
+    return new Bs.View.Alert({
+      type     : 'success',
+      title    : 'success.title',
+      msg      : msg || 'success.msg.default',
+      renderTo : renderTo || 'body',
+      translate: !translated
+    });
+  };
 
-	Bs.View.Alert.info = function (msg, renderTo, translated) {
-		return new Bs.View.Alert({
-			type     : 'info',
-			title    : 'info.title',
-			msg      : msg,
-			renderTo : renderTo || 'body',
-			translate: !translated
-		});
-	};
+  Bs.View.Alert.info = function (msg, renderTo, translated) {
+    return new Bs.View.Alert({
+      type     : 'info',
+      title    : 'info.title',
+      msg      : msg,
+      renderTo : renderTo || 'body',
+      translate: !translated
+    });
+  };
 
-	Bs.View.Alert.apiProblem = function (response, renderTo) {
-		console.log(response)
-		if(response && response !== null && response.status && response.detail && response.title) {
-			return new Bs.View.Alert({
-				type          : (response.status >= 500) ? "danger" : "warning",
-				msg           : response.detail,
-				translate     : false,
-				translateTitle: false,
-				title         : response.title,
-				delay         : 30000,
-				renderTo      : renderTo || 'body'
-			});
-		}
-		else {
-			return Bs.View.Alert.error(null, renderTo);
-		}
-	}
+  Bs.View.Alert.apiProblem = function (response, renderTo) {
+    console.log(response)
+    if(response && response !== null && response.status && response.detail && response.title) {
+      return new Bs.View.Alert({
+        type          : (response.status >= 500) ? "danger" : "warning",
+        msg           : response.detail,
+        translate     : false,
+        translateTitle: false,
+        title         : response.title,
+        delay         : 30000,
+        renderTo      : renderTo || 'body'
+      });
+    }
+    else {
+      return Bs.View.Alert.error(null, renderTo);
+    }
+  }
 
 });
-
 "use strict";
 
 Bs.define('Bs.View.Alert.Toast', {
@@ -8213,15 +8200,15 @@ Bs.define('Bs.Util.UrlParser', {
 	};
 });
 
-+function(){Bs.Lang.addResourceBundle('en', 'Bs/View/Alert', {"info":{"title":"Information","msg":{"nothing":"Nothing to modify"}},"success":{"title":"Finished","msg":{"saved":"Saved","default":"Operation successful"}},"warning":{"title":"Warning"},"danger":{"title":"Error","msg":{"notSaved":"Modifications not saved","notFetched":"Unexpected data received from server","default":"An error has occurred"}}})}();
++function(){Bs.Lang.addResourceBundle('en', 'Bs/View/Alert', {"info":{"title":"Information","msg":{"nothing":"Nothing to modify"}},"success":{"title":"Finished","msg":{"saved":"Saved","default":"Operation successful"}},"warning":{"title":"Warning"},"danger":{"title":"Oops","msg":{"notSaved":"Modifications not saved","notFetched":"Unexpected data received from server","default":"An error has occurred"}}})}();
 +function(){Bs.Lang.addResourceBundle('en', 'Bs/View/Collection/Empty', {"nothingToDisplay":"Nothing to display..."})}();
 +function(){Bs.Lang.addResourceBundle('en', 'Bs/View/Collection/Error', {"error":"Oops, something wrong happened there..."})}();
 +function(){Bs.Lang.addResourceBundle('en', 'Bs/View/Modal', {})}();
-+function(){Bs.Lang.addResourceBundle('fr', 'Bs/View/Alert', {"info":{"title":"Information","msg":{"nothing":"Rien à modifier"}},"success":{"title":"Terminé","msg":{"saved":"Modification effectuée","default":"Opération réussie"}},"warning":{"title":"Attention"},"danger":{"title":"Erreur","msg":{"notSaved":"Les modifications n'ont pas pu être enregistrées","notFetched":"Le serveur n'a pas renvoyé les données attendues","default":"Une erreur est survenue"}}})}();
++function(){Bs.Lang.addResourceBundle('fr', 'Bs/View/Alert', {"info":{"title":"Information","msg":{"nothing":"Rien à modifier"}},"success":{"title":"Terminé","msg":{"saved":"Modification effectuée","default":"Opération réussie"}},"warning":{"title":"Attention"},"danger":{"title":"Oups","msg":{"notSaved":"Les modifications n'ont pas pu être enregistrées","notFetched":"Le serveur n'a pas renvoyé les données attendues","default":"Une erreur est survenue"}}})}();
 +function(){Bs.Lang.addResourceBundle('fr', 'Bs/View/Collection/Empty', {"nothingToDisplay":"Rien à afficher pour le moment..."})}();
 +function(){Bs.Lang.addResourceBundle('fr', 'Bs/View/Collection/Error', {"error":"Oups, il y a eu un problème..."})}();
 +function(){Bs.Lang.addResourceBundle('fr', 'Bs/View/Modal', {})}();
-+function(){Bs.Lang.addResourceBundle('nl', 'Bs/View/Alert', {"info":{"title":"Informatie","msg":{"nothing":"Niets te wijzigen"}},"success":{"title":"Klaar","msg":{"saved":"Wijziging doorgevoerd","default":"Met succes uitgevoerd"}},"warning":{"title":"Waarschuwing"},"danger":{"title":"Fout","msg":{"notSaved":"De wijzigingen konden niet opgeslagen worden","notFetched":"De server heeft de verwachte gegevens niet teruggestuurd","default":"Er is een fout opgetreden"}}})}();
++function(){Bs.Lang.addResourceBundle('nl', 'Bs/View/Alert', {"info":{"title":"Informatie","msg":{"nothing":"Niets te wijzigen"}},"success":{"title":"Klaar","msg":{"saved":"Wijziging doorgevoerd","default":"Met succes uitgevoerd"}},"warning":{"title":"Waarschuwing"},"danger":{"title":"Oops","msg":{"notSaved":"De wijzigingen konden niet opgeslagen worden","notFetched":"De server heeft de verwachte gegevens niet teruggestuurd","default":"Er is een fout opgetreden"}}})}();
 +function(){Bs.Lang.addResourceBundle('nl', 'Bs/View/Collection/Empty', {"nothingToDisplay":"Momenteel niets te tonen…"})}();
 +function(){Bs.Lang.addResourceBundle('nl', 'Bs/View/Collection/Error', {"error":"Oops, something wrong happened there..."})}();
 +function(){Bs.Lang.addResourceBundle('nl', 'Bs/View/Modal', {})}();
--- src/Bs/View/Alert/Alert.js
+++ src/Bs/View/Alert/Alert.js
@@ -1,208 +1,196 @@
 "use strict";
 
 Bs.define('Bs.View.Alert', {
-	extend       : 'Bs.View',
-	hasStylesheet: false,
-	autoMask     : false,
-	elStyle      : {position: 'relative'},
-	elClass      : "bootstrap",
-	options      : {
-		translate      			: true,
-		translateTitle 			: true,
-		icon           			: null,
-		type           			: 'info',
-		title          			: null,
-		hasTitle       			: true,
-		msg            			:	null,
-		view           			: null,
-		viewOptions    			: null,
-		dismissible    			: true,
-		autoDismissible			: false,
-		cancelDismissOnHover: true,
-		delay          			: 4000
-	},
-	data					: {
-		idTimeout : null
-	},
+  extend       : 'Bs.View',
+  hasStylesheet: false,
+  autoMask     : false,
+  elStyle      : {position: 'relative'},
+  elClass      : "bootstrap",
+  options      : {
+    translate      : true,
+    translateTitle : true,
+    icon           : null,
+    type           : 'info',
+    title          : null,
+    hasTitle       : true,
+    msg            : null,
+    view           : null,
+    viewOptions    : null,
+    dismissible    : true,
+    autoDismissible: false,
+    delay          : 4000
+  },
 
-	initialize: function () {
-		var me = this;
-		me.callParent("initialize");
-		if (!me.options.title) {
-			if (me.options.type === 'default') {
-				me.options.hasTitle = false;
-			}
-			else {
-				me.options.title = me.options.type + '.title';
-			}
-		}
+  initialize: function () {
+    var me = this;
+    me.callParent("initialize");
+    if (!me.options.title) {
+      if (me.options.type === 'default') {
+        me.options.hasTitle = false;
+      }
+      else {
+        me.options.title = me.options.type + '.title';
+      }
+    }
 
-		if (!me.options.icon) {
-			switch (me.options.type) {
-				case 'danger':
-					me.options.icon = 'fa fa-exclamation-circle';
-					break;
-				case 'warning':
-					me.options.icon = 'fa fa-exclamation-triangle';
-					break;
-				case 'success':
-					me.options.icon = 'fa fa-check';
-					break;
-				case 'info':
-					me.options.icon = 'fa fa-info-circle';
-					break;
-				default:
-					me.options.icon = "fa fa-" + me.options.type;
-			}
-		}
-		if (me.options.type === 'danger') {
-			me.options.autoDismissible = false;
-		}
-		if (me.options.view) {
-			me.options.translate = false;
-		}
-		else {
-			if (me.options.msg instanceof jQuery) {
-				me.options.msg = $('<div></div>').html(me.options.msg).html();
-				me.options.translate = false;
-			}
-			else {
-				if ($.isArray(me.options.msg)) {
-					me.options.msg = me.options.msg[0].urlPath + ':' + me.options.msg[1]
-				}
-				else if (!me.options.msg) {
-					if (me.options.type === "success") {
-						me.options.msg = Bs.Lang.t("success_");
-					}
-					else {
-						me.options.msg = Bs.Lang.t('unknown_error');
-					}
-				}
-			}
-		}
-		if ($.isArray(me.options.title)) {
-			me.options.title = me.options.title[0].urlPath + ':' + me.options.title[1]
-		}
-	},
-
-	render: function () {
-		var me = this;
-		me.callParent("render");
-		me.$el.hide();
-
-		if (me.options.view) {
-			Bs.require(me.options.view, function (View) {
-				var view,
-					viewOptions = me.options.viewOptions || {};
-
-				viewOptions.renderTo = me.$el.find('.content');
-				view = new View(viewOptions);
-				me.renderCallback();
-				view.on('close', function () {
-					me.trigger('close');
-				});
-			})
-		}
-		me.renderCallback();
-		me.trigger("ready");
-	},
-
-	renderCallback: function () {
-		var me = this;
-
-		me.$el.slideDown(function () {
-			me.trigger('shown');
-			me.$el.find('.alert-icon').fadeIn();
-		});
-		if (me.options.autoDismissible) {
-			me.data.idTimeout = setTimeout(function () {
-				me.close();
-			}, me.options.delay)
-			if(me.options.cancelDismissOnHover) {
-				me.$el.on('mouseover', function() {
-					if(me.data.idTimeout) {
-            clearTimeout(me.data.idTimeout)
-            me.data.idTimeout = null;
+    if (!me.options.icon) {
+      switch (me.options.type) {
+        case 'danger':
+          me.options.icon = 'fa fa-exclamation-circle';
+          break;
+        case 'warning':
+          me.options.icon = 'fa fa-exclamation-triangle';
+          break;
+        case 'success':
+          me.options.icon = 'fa fa-check';
+          break;
+        case 'info':
+          me.options.icon = 'fa fa-info-circle';
+          break;
+        default:
+          me.options.icon = "fa fa-" + me.options.type;
+      }
+    }
+    if (me.options.type === 'danger') {
+      me.options.autoDismissible = false;
+    }
+    if (me.options.view) {
+      me.options.translate = false;
+    }
+    else {
+      if (me.options.msg instanceof jQuery) {
+        me.options.msg = $('<div></div>').html(me.options.msg).html();
+        me.options.translate = false;
+      }
+      else {
+        if ($.isArray(me.options.msg)) {
+          me.options.msg = me.options.msg[0].urlPath + ':' + me.options.msg[1]
+        }
+        else if (!me.options.msg) {
+          if (me.options.type === "success") {
+            me.options.msg = Bs.Lang.t("success_");
           }
-				})
-			}
-		}
-	},
+          else {
+            me.options.msg = Bs.Lang.t('unknown_error');
+          }
+        }
+      }
+    }
+    if ($.isArray(me.options.title)) {
+      me.options.title = me.options.title[0].urlPath + ':' + me.options.title[1]
+    }
+  },
 
-	close: function (e) {
-		if (e) {
-			e.preventDefault();
-			e.stopPropagation();
-		}
-		var me = this;
-		me.$el.find('.alert-icon').hide();
-		this.$el.slideUp(function () {
-			me.$el.remove();
-		});
-		return false;
-	},
+  render: function () {
+    var me = this;
+    me.callParent("render");
+    me.$el.hide();
 
-	events: {
-		"click .close": "close"
-	}
+    if (me.options.view) {
+      Bs.require(me.options.view, function (View) {
+        var view,
+          viewOptions = me.options.viewOptions || {};
+
+        viewOptions.renderTo = me.$el.find('.content');
+        view = new View(viewOptions);
+        me.renderCallback();
+        view.on('close', function () {
+          me.trigger('close');
+        });
+      })
+    }
+    me.renderCallback();
+    me.trigger("ready");
+  },
+
+  renderCallback: function () {
+    var me = this;
+
+    me.$el.slideDown(function () {
+      me.trigger('shown');
+      me.$el.find('.alert-icon').fadeIn();
+    });
+    if (me.options.autoDismissible) {
+      setTimeout(function () {
+        me.close();
+      }, me.options.delay)
+    }
+  },
+
+  close: function (e) {
+    if (e) {
+      e.preventDefault();
+      e.stopPropagation();
+    }
+    var me = this;
+    me.$el.find('.alert-icon').hide();
+    this.$el.slideUp(function () {
+      me.$el.remove();
+    });
+    return false;
+  },
+
+  events: {
+    "click .close": "close"
+  }
 
 }, function () {
-	Bs.View.Alert.error = function (msg, renderTo, translated) {
-		return new Bs.View.Alert({
-			type     : 'danger',
-			title    : 'danger.title',
-			msg      : msg || 'danger.msg.default',
-			renderTo : renderTo || 'body',
-			translate: !translated
-		});
-	};
+  Bs.View.Alert.error = function (msg, renderTo, translated) {
+    return new Bs.View.Alert({
+      type     : 'danger',
+      title    : 'danger.title',
+      msg      : msg || 'danger.msg.default',
+      renderTo : renderTo || 'body',
+      translate: !translated
+    });
+  };
 
-	Bs.View.Alert.warning = function (msg, renderTo, translated) {
-		return new Bs.View.Alert({
-			type     : 'warning',
-			title    : 'warning.title',
-			msg      : msg,
-			renderTo : renderTo || 'body',
-			translate: !translated
-		});
-	};
+  Bs.View.Alert.warning = function (msg, renderTo, translated) {
+    return new Bs.View.Alert({
+      type     : 'warning',
+      title    : 'warning.title',
+      msg      : msg,
+      renderTo : renderTo || 'body',
+      translate: !translated
+    });
+  };
 
-	Bs.View.Alert.success = function (msg, renderTo, translated) {
-		return new Bs.View.Alert({
-			type     : 'success',
-			title    : 'success.title',
-			msg      : msg || 'success.msg.default',
-			renderTo : renderTo || 'body',
-			translate: !translated
-		});
-	};
+  Bs.View.Alert.success = function (msg, renderTo, translated) {
+    return new Bs.View.Alert({
+      type     : 'success',
+      title    : 'success.title',
+      msg      : msg || 'success.msg.default',
+      renderTo : renderTo || 'body',
+      translate: !translated
+    });
+  };
 
-	Bs.View.Alert.info = function (msg, renderTo, translated) {
-		return new Bs.View.Alert({
-			type     : 'info',
-			title    : 'info.title',
-			msg      : msg,
-			renderTo : renderTo || 'body',
-			translate: !translated
-		});
-	};
+  Bs.View.Alert.info = function (msg, renderTo, translated) {
+    return new Bs.View.Alert({
+      type     : 'info',
+      title    : 'info.title',
+      msg      : msg,
+      renderTo : renderTo || 'body',
+      translate: !translated
+    });
+  };
 
-	Bs.View.Alert.apiProblem = function (response, renderTo) {
-		console.log(response)
-		if(response && response !== null && response.status && response.detail && response.title) {
-			return new Bs.View.Alert({
-				type          : (response.status >= 500) ? "danger" : "warning",
-				msg           : response.detail,
-				translate     : false,
-				translateTitle: false,
-				title         : response.title,
-				delay         : 30000,
-				renderTo      : renderTo || 'body'
-			});
-		}
-		else {
-			return Bs.View.Alert.error(null, renderTo);
-		}
-	}
+  Bs.View.Alert.apiProblem = function (response, renderTo) {
+    console.log(response)
+    if(response && response !== null && response.status && response.detail && response.title) {
+      return new Bs.View.Alert({
+        type          : (response.status >= 500) ? "danger" : "warning",
+        msg           : response.detail,
+        translate     : false,
+        translateTitle: false,
+        title         : response.title,
+        delay         : 30000,
+        renderTo      : renderTo || 'body'
+      });
+    }
+    else {
+      return Bs.View.Alert.error(null, renderTo);
+    }
+  }
 
 });
